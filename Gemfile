a1fc47f8da99cc25033b8fd29ad9452104f6306d
nasreddine ghribi <nas.ghribi@gmail.com>
2021-08-13 11:19:54 +0200

    changed the YML back

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
-
+gem 'dotenv-rails', groups: [:development, :test]
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
@@ -46,3 +46,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+gem 'autoprefixer-rails', '10.2.5'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'simple_form'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   # get '/lists', to: 'lists#index'
+#   # get '/lists/new', to: 'lists#new'
+#   # post '/lists', to: 'lists#create'
+#   # get '/lists/:id', to: 'lists#show', as: 'list'
+#   # get 'lists/:list_id/bookmarks/new', to: 'bookmarks#new'
+#   # post 'lists/:list_id/bookmarks', to: 'bookmarks#create'
+#   # delete '/bookmarks/:id', to: 'bookmarks#destroy'
+#   resources :lists, only: [:index, :new, :create, :show] do
+#     resources :bookmarks, only: [:new, :create, :destroy]
+#   end
+# end
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
 end
